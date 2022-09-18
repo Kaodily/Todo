533da52ab995bfa5edca8cb16c2c9f091dbe8c2f
Kaodily <100316396+Kaodily@users.noreply.github.com>
2022-09-18 11:08:04 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import './App.css';
+
+function App() {
+  return (
+    <Fragment className="App">
+     
+    </Fragment>
+  );
+}
+
+export default App;
